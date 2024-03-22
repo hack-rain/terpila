701df8c176e32e9322f776000eaf6a1418eef564
hack-rain <158817272+hack-rain@users.noreply.github.com>
2024-03-22 15:35:49 +0200

    Update terpila.py

--- terpila.py
+++ terpila.py
@@ -16,7 +16,7 @@ app.config['SESSION_TYPE'] = 'filesystem'
 app.config['SESSION_FILE_DIR'] = "session_data"
 app.config['PERMANENT_SESSION_LIFETIME'] = 10
 Session(app)
-CORS(app)
+CORS(app, support_credentials=True)
 
 tg_creds = ConfigParser()
 tg_creds.read('config.ini')
